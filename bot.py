8344ac12604a9cd63f8c0ebdaea6d1b354e5ea0d
Kaki <VolkerS2006@web.de>
2021-02-21 18:32:07 +0100

    Switched to aiosqlite

--- bot.py
+++ bot.py
@@ -1,19 +1,40 @@
+from sqlite3.dbapi2 import sqlite_version
 import discord
 import discord.ext.commands as commands
 import requests
 import time
 import asyncio
-import sqlite3 as sqlite
-
-# init database
-connection = sqlite.connect("bot.db")
-db = connection.cursor()
+import aiosqlite as sqlite
 
 
-try:
-    db.execute("""CREATE TABLE prefixes (guild_id INTEGER, prefix STRING)""")
-except sqlite.OperationalError as err:
-    print("Database existing, using that one.")
+async def init_db():
+    db = await sqlite.connect("bot.db")
+
+    try:
+        await db.execute("CREATE TABLE prefixes (guild_id INTEGER, prefix STRING)")
+    except sqlite.OperationalError as err:
+        print("Database existing, using that one.")
+    await db.close()
+
+
+async def get_prefix(client, message):
+    try:
+        guild = message.guild
+    except AttributeError:
+        guild = message
+    try:
+        db = await sqlite.connect("bot.db")
+        await db.execute("SELECT prefix FROM prefixes WHERE guild_id=?", guild.id)
+        result = await db.fetchall()
+        await db.close()
+    except:
+        return ">"
+
+
+bot = commands.AutoShardedBot(
+    command_prefix=get_prefix, case_insensitive=True, owner_ids=[686941073792303157686941073792303157])
+
+bot.loop.create_task(init_db())
 
 with open("./data/botsggtoken.txt") as token:
     BOTS_GG_TOKEN = token.readline()
@@ -27,28 +48,8 @@ class Colours:
     warn: discord.Color = discord.Color(16707936)
 
 
-def get_prefix(client, message):
-    try:
-        guild = message.guild
-    except AttributeError:
-        guild = message
-    try:
-        db.execute("SELECT prefix FROM prefixes WHERE guild_id=?", guild.id)
-        return db.fetchall()
-    except:
-        return ">"
-
-
-bot = commands.AutoShardedBot(
-    command_prefix=get_prefix, case_insensitive=True, owner_ids=[686941073792303157686941073792303157])
-
-
 @bot.event
 async def on_message(message):
-    """
-    Check if the user pinged the bot; if they did, tell them the bot's prefix.
-    """
-
     if message.content.startswith("<@!749301838859337799>"):
         prefix = get_prefix(bot, message)
         await message.channel.send(f"{message.author.mention}, this server's prefix is `{prefix}`. Try `{prefix}help`"
@@ -61,13 +62,6 @@ async def on_message(message):
 
 @bot.event
 async def on_guild_join(guild):
-    """
-    Send a message on guild join, then create a webhook to be used in the replace command.
-
-    :param guild: the guild that was joined
-    :return: N/A
-    """
-
     # create the join embed
     embed = discord.Embed(
         title="Hi!",
@@ -131,16 +125,6 @@ async def on_ready():
 
 
 async def send_error(ctx, err, extra_info=None, full_error=None):
-    """
-    Send an error message to a specified channel.
-
-    :param ctx: context
-    :param err: the error string
-    :param extra_info: any extra info that the user might want to know
-    :param full_error: the full error
-    :return: N/A
-    """
-
     error_embed = discord.Embed()
     error_embed.colour = Colours.fail
     error_embed.description = err
